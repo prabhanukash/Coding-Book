76fe399e3d6c9606af6f61375c7bc81b5b3d6a4b
prabhanukash <prakashbhanu019@gmail.com>
2021-05-12 23:16:20 +0530

    prims Algorithm added

--- Graphs/Prims.cpp
+++ Graphs/Prims.cpp
@@ -41,7 +41,46 @@ void solve()
 	{
 		ll u, v, w;
 		cin >> u >> v >> w;
+		adj[u].pb({v, w});
+		adj[v].pb({u, w});
 	}
+	ll ans = 0;
+
+	vi minEdge(n + 1, inf), inc(n + 1);
+
+	set<pii> s; //{min_w,u}
+	s.insert({0, 1});
+	minEdge[1] = 0;
+
+	for (ll i = 1; i <= n; ++i)
+	{
+		if (s.empty())
+			return -1;
+
+		auto it = s.begin();
+		ll u = it->second;
+
+		s.erase(it);
+
+		inc[u] = 1;
+		ans += minEdge[u];
+
+		for (auto &e : adj[u])
+		{
+			ll v = e.first, w = e.second;
+
+			if (inc[v])
+				continue;
+
+			if (w < minEdge[v])
+			{
+				s.erase({minEdge[v], v});
+				minEdge[v] = w;
+				s.insert({minEdge[v], v});
+			}
+		}
+	}
+
 }
 
 signed main()
