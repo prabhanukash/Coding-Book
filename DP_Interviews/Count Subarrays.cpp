94a175e667f50784207abc9fe77d3240f5d44bf0
prabhanukash <prakashbhanu019@gmail.com>
2021-05-22 10:20:13 +0530

    first commit

---
+++ DP_Interviews/Count Subarrays.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define ll long long int
+#define ff first
+#define ss second
+#define S size()
+#define mod (ll)(1e9 + 7)
+#define mset(a, k) memset(a, k, sizeof(a))
+#define fr(i, x, y) for (ll i = x; i < y; i++)
+#define dr(i, x, y) for (ll i = x; i >= y; i--)
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+#define mapcl map<char, ll>
+#define mapll map<ll, ll>
+#define mapsl map<string, ll>
+#define vi vector<ll>
+#define vs vector<string>
+#define vb vector<bool>
+#define psi pair<string, ll>
+#define pii pair<ll, ll>
+#define piii pair<ll, pii>
+#define vii vector<pii>
+#define vvi vector<vi>
+#define vvii vector<vii>
+using namespace std;
+void fast()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+//----------------------------------------FUNCTIONS-------------------------------------
+const ll N = (ll)(1 * 1e6 + 5);
+
+void solve()
+{
+	ll n;
+	cin >> n;
+	ll a[n];
+	ll lis[n];
+	fr(i, 0, n)cin >> a[i];
+	fr(i, 0, n)
+	{
+		ll mx = 0;
+		fr(j, 0, i)
+		{
+			if (a[j] <= a[i])
+			{
+				if (lis[j] > mx)mx = lis[j];
+			}
+		}
+		if (mx == 0)lis[i] = 1;
+		else lis[i] = mx + 1;
+	}
+	fr(i, 0, n)cout << lis[i] << ' ';
+	cout << '\n';
+}
+
+signed main()
+{
+	fast();
+#ifndef ONLINE_JUDGE
+	freopen ("inp.txt", "r", stdin);
+	freopen ("out.txt", "w", stdout);
+#endif
+	ll t = 1;
+//	cin >> t;
+	fr(i, 0, t)
+	{
+		solve();
+	}
+}
