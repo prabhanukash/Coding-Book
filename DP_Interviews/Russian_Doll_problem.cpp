f39244433c899a5bc820b9787ad923b3a908a5b7
prabhanukash <prakashbhanu019@gmail.com>
2021-08-03 14:40:43 +0530

    LIS variations commited

--- DP_Interviews/Russian_Doll_problem.cpp
+++ DP_Interviews/Russian_Doll_problem.cpp
@@ -1,4 +1,10 @@
+// Problem Link - https://leetcode.com/problems/russian-doll-envelopes/
+/* By Bhanu Prakash */
 #include <bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/trie_policy.hpp>
+//using namespace __gnu_pbds;
 #define pb push_back
 #define mp make_pair
 #define ll long long int
@@ -6,14 +12,13 @@
 #define ss second
 #define S size()
 #define mod (ll)(1e9 + 7)
-#define mset(a, k) memset(a, k, sizeof(a))
+#define inf 1e18
 #define fr(i, x, y) for (ll i = x; i < y; i++)
 #define dr(i, x, y) for (ll i = x; i >= y; i--)
 #define all(v) v.begin(), v.end()
 #define allr(v) v.rbegin(), v.rend()
 #define mapcl map<char, ll>
 #define mapll map<ll, ll>
-#define mapsl map<string, ll>
 #define vi vector<ll>
 #define vs vector<string>
 #define vb vector<bool>
@@ -24,6 +29,8 @@
 #define vvi vector<vi>
 #define vvii vector<vii>
 using namespace std;
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+//typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
 void fast()
 {
 	ios_base::sync_with_stdio(false);
@@ -32,43 +39,32 @@ void fast()
 }
 //----------------------------------------FUNCTIONS-------------------------------------
 const ll N = (ll)(1 * 1e6 + 5);
-/*
-https://leetcode.com/problems/russian-doll-envelopes/
-*/
-ll LIS(vii &a)
-{
-	ll n = a.S;
-	ll lis[n];
-	ll maxi = 0;
-	fr(i, 0, n)
-	{
-		ll mx = 0;
-		fr(j, 0, i)
-		{
-			if (a[j].ss < a[i].ss and a[j].ff < a[i].ff)
-			{
-				if (mx < lis[j])mx = lis[j];
-			}
-		}
-		lis[i] = 1 + mx;
-		maxi = max(maxi, lis[i]);
-	}
-	return maxi;
-}
+
 void solve()
 {
 	ll n;
 	cin >> n;
 	vii a(n);
-	fr(i, 0, n)
-	{
-		cin >> a[i].ff >> a[i].ss;
-	}
+	fr(i, 0, n)cin >> a[i].ff >> a[i].ss;
 	sort(all(a));
-	cout << LIS(a) << '\n';
-
+	ll dp[n];
+	dp[0] = 1;
+	ll ans = 0;
+	fr(i, 1, n)
+	{
+		ll mx = 0;
+		fr(j, 0, i)
+		{
+			if (a[j].ss < a[i].ss)
+			{
+				mx = max(mx, dp[j]);
+			}
+		}
+		dp[i] = mx + 1;
+		ans = max(ans, dp[i]);
+	}
+	cout << ans << '\n';
 }
-
 
 signed main()
 {
