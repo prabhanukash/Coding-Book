01531d70fd4a04e10b8df4c87031d1b598b33f4e
prabhanukash <prakashbhanu019@gmail.com>
2021-08-03 15:02:09 +0530

    least matrix path added

--- DP_Interviews/least_matirx_path.cpp
+++ DP_Interviews/least_matirx_path.cpp
@@ -1,4 +1,10 @@
+// Problem Link - https://www.geeksforgeeks.org/min-cost-path-dp-6/
+/* By Bhanu Prakash */
 #include <bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/trie_policy.hpp>
+//using namespace __gnu_pbds;
 #define pb push_back
 #define mp make_pair
 #define ll long long int
@@ -6,14 +12,13 @@
 #define ss second
 #define S size()
 #define mod (ll)(1e9 + 7)
-#define mset(a, k) memset(a, k, sizeof(a))
+#define inf 1e18
 #define fr(i, x, y) for (ll i = x; i < y; i++)
 #define dr(i, x, y) for (ll i = x; i >= y; i--)
 #define all(v) v.begin(), v.end()
 #define allr(v) v.rbegin(), v.rend()
 #define mapcl map<char, ll>
 #define mapll map<ll, ll>
-#define mapsl map<string, ll>
 #define vi vector<ll>
 #define vs vector<string>
 #define vb vector<bool>
@@ -24,6 +29,8 @@
 #define vvi vector<vi>
 #define vvii vector<vii>
 using namespace std;
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+//typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
 void fast()
 {
 	ios_base::sync_with_stdio(false);
@@ -32,10 +39,7 @@ void fast()
 }
 //----------------------------------------FUNCTIONS-------------------------------------
 const ll N = (ll)(1 * 1e6 + 5);
-ll a[1000][100];
-/*
-https://www.geeksforgeeks.org/min-cost-path-dp-6/
-*/
+
 ll dp[1000][1000];
 ll least_cost_path( ll n, ll m)
 {
@@ -44,6 +48,7 @@ ll least_cost_path( ll n, ll m)
 	if (n == 0 and m == 0)return a[0][0];
 	return dp[n][m] = a[n - 1][m - 1] + min({least_cost_path( n - 1, m), least_cost_path( n - 1, m - 1), least_cost_path( n, m - 1)});
 }
+
 void solve()
 {
 	ll n, m;
