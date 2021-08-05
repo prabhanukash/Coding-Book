cecfdb3d7ce88c8042427f72d5b0b2fa633fc01a
prabhanukash <prakashbhanu019@gmail.com>
2021-08-05 23:42:14 +0530

    binary Trees problems added

---
+++ Binary_Trees/Good_Nodes.cpp
@@ -0,0 +1,86 @@
+// Problem Link -
+/* By Bhanu Prakash */
+#include <bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/trie_policy.hpp>
+//using namespace __gnu_pbds;
+#define pb push_back
+#define mp make_pair
+#define ll long long int
+#define ff first
+#define ss second
+#define S size()
+#define mod (ll)(1e9 + 7)
+#define inf 1e18
+#define fr(i, x, y) for (ll i = x; i < y; i++)
+#define dr(i, x, y) for (ll i = x; i >= y; i--)
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+#define mapcl map<char, ll>
+#define mapll map<ll, ll>
+#define vi vector<ll>
+#define vs vector<string>
+#define vb vector<bool>
+#define psi pair<string, ll>
+#define pii pair<ll, ll>
+#define piii pair<ll, pii>
+#define vii vector<pii>
+#define vvi vector<vi>
+#define vvii vector<vii>
+using namespace std;
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+//typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
+void fast()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+//----------------------------------------FUNCTIONS-------------------------------------
+const ll N = (ll)(1 * 1e6 + 5);
+class Node
+{
+public:
+	ll data;
+	Node* left;
+	Node * right;
+	Node(ll d)
+	{
+		this->data = d;
+		this->left = this->right = NULL;
+	}
+};
+ll traverse(Node* root, ll mx_val)
+{
+	if (root == nullptr)return 0;
+	ll res = root->data >= mx_val ? 1 : 0;
+	res += traverse(root->left, mx_val);
+	res += traverse(root->right, mx_val);
+	return res;
+}
+void solve()
+{
+	Node* root = new Node(3);
+	root->left = new Node(1);
+	root->right = new Node(4);
+	root->left->left = new Node(3);
+	root->right->right = new Node(5);
+	root->right->left = new Node(1);
+	cout << traverse(root, root->data) << '\n';
+}
+
+signed main()
+{
+	fast();
+#ifndef ONLINE_JUDGE
+	freopen ("inp.txt", "r", stdin);
+	freopen ("out.txt", "w", stdout);
+#endif
+	ll t = 1;
+//	cin >> t;
+	fr(i, 0, t)
+	{
+		solve();
+	}
+}
