a99b81743e8897aa1bb6edfb95d2eead39622144
prabhanukash <prakashbhanu019@gmail.com>
2021-08-22 15:18:56 +0530

    first commit

---
+++ Two_Pointers/max_substring.cpp
@@ -0,0 +1,79 @@
+// Problem Link -
+/* By Bhanu Prakash */
+#include <bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/trie_policy.hpp>
+//using namespace __gnu_pbds;
+#define pb push_back
+#define mp make_pair
+#define ll long long int
+#define ff first
+#define ss second
+#define S size()
+#define mod (ll)(1e9 + 7)
+#define inf 1e18
+#define fr(i, x, y) for (ll i = x; i < y; i++)
+#define dr(i, x, y) for (ll i = x; i >= y; i--)
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+#define mapcl map<char, ll>
+#define mapll map<ll, ll>
+#define vi vector<ll>
+#define vs vector<string>
+#define vb vector<bool>
+#define psi pair<string, ll>
+#define pii pair<ll, ll>
+#define piii pair<ll, pii>
+#define vii vector<pii>
+#define vvi vector<vi>
+#define vvii vector<vii>
+using namespace std;
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+//typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
+void fast()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+//----------------------------------------FUNCTIONS-------------------------------------
+const ll N = (ll)(1 * 1e6 + 5);
+
+void solve()
+{
+    string s;
+    cin >> s;
+    // we need to find the max substring with unique char
+    set<char> st;
+    ll j = 0; // two pointers approach from brute force
+    ll ans = 0;
+    fr(i, 0, s.S)
+    {
+        for (; j < s.S; j++)
+        {
+            if (st.find(s[j]) == st.end())
+                st.insert(s[j]);
+            else
+                break;
+        }
+        ans = max(ans, (ll)st.S);
+        st.erase(s[i]);
+    }
+    cout << ans << '\n';
+}
+
+signed main()
+{
+    fast();
+#ifndef ONLINE_JUDGE
+    freopen("inp.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    ll t = 1;
+    //cin >> t;
+    fr(i, 0, t)
+    {
+        solve();
+    }
+}
