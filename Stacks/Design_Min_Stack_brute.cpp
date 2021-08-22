82810ef0af02e85c607d8f6eb1423e6bbc353c5f
prabhanukash <prakashbhanu019@gmail.com>
2021-08-22 12:29:09 +0530

    stack problems added

---
+++ Stacks/Design_Min_Stack_brute.cpp
@@ -0,0 +1,115 @@
+// Problem Link -
+/* By Bhanu Prakash */
+#include <bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/trie_policy.hpp>
+//using namespace __gnu_pbds;
+#define pb push_back
+#define mp make_pair
+#define ll long long int
+#define ff first
+#define ss second
+#define S size()
+#define mod (ll)(1e9 + 7)
+#define inf 1e18
+#define fr(i, x, y) for (ll i = x; i < y; i++)
+#define dr(i, x, y) for (ll i = x; i >= y; i--)
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+#define mapcl map<char, ll>
+#define mapll map<ll, ll>
+#define vi vector<ll>
+#define vs vector<string>
+#define vb vector<bool>
+#define psi pair<string, ll>
+#define pii pair<ll, ll>
+#define piii pair<ll, pii>
+#define vii vector<pii>
+#define vvi vector<vi>
+#define vvii vector<vii>
+using namespace std;
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+//typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
+void fast()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+//----------------------------------------FUNCTIONS-------------------------------------
+const ll N = (ll)(1 * 1e6 + 5);
+class MinStack
+{
+public:
+	stack<ll> alldata;
+	stack<ll> mindata;
+	ll size()//O(1)
+	{
+		return alldata.S;
+	}
+	void push(ll d)//O(1)
+	{
+		alldata.push(d);
+		if (mindata.S == 0 or mindata.top() >= d)
+		{
+			mindata.push(d);
+		}
+	}
+	void pop()//O(1)
+	{
+		if (alldata.S == 0)
+		{
+			cout << "Stack Underflow\n";
+			return;
+		}
+		if (alldata.top() == mindata.top())mindata.pop();
+		alldata.pop();
+
+	}
+	ll top()//O(1)
+	{
+		if (alldata.S == 0)
+		{
+			cout << "Stack Underflow\n";
+			return -1;
+		}
+		return alldata.top();
+	}
+	ll min()//O(1)
+	{
+		if (mindata.S == 0)
+		{
+			cout << "Stack Underflow\n";
+			return -1;
+		}
+		else return mindata.top();
+	}
+};//S=>O(n)
+void solve()
+{
+	MinStack st;
+	st.push(1);
+	st.push(2);
+	st.push(0);
+	st.push(4);
+	cout << st.min() << '\n';
+	st.pop();
+	st.pop();
+	cout << st.min() << '\n';
+}
+
+signed main()
+{
+	fast();
+#ifndef ONLINE_JUDGE
+	freopen ("inp.txt", "r", stdin);
+	freopen ("out.txt", "w", stdout);
+#endif
+	ll t = 1;
+//	cin >> t;
+	fr(i, 0, t)
+	{
+		solve();
+	}
+}
