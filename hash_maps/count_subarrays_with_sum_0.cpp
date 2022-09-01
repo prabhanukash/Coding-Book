e3ea5465be730b38a1c5ba9871d08ded7d67f214
Bhanu-Prakash <Bhanu.Prakash@247.ai>
2022-09-01 16:09:54 +0530

    heap problems added

--- hash_maps/count_subarrays_with_sum_0.cpp
+++ hash_maps/count_subarrays_with_sum_0.cpp
@@ -4,21 +4,22 @@ using namespace std;
 int main()
 {
 #ifndef ONLINE_JUDGE
-		freopen("inp.txt", "r", stdin);
-		freopen("out.txt", "w", stdout);
+	freopen("inp.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
 #endif
 	int n;
-	cin>>n;
+	cin >> n;
 	int arr[n];
 	map<int, int> mp;
 	mp[0] = 1;
-	for (int i = 0; i < n;i++)
+	for (int i = 0; i < n; i++)
 	{
 		cin >> arr[i];
 	}
 	int ps = 0;
 	int ans = 0;
-	for(int i = 0; i < n; i++){
+	for (int i = 0; i < n; i++)
+	{
 		ps += arr[i];
 		ans += mp[ps];
 		mp[ps]++;
