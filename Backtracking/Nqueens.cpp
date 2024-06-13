6db85fe899177f51bd750a4232e8a783447be934
prabhanukash <prakashbhanu019@gmail.com>
2024-06-13 09:29:04 +0530

    NQueens modification changes

--- Backtracking/Nqueens.cpp
+++ Backtracking/Nqueens.cpp
@@ -1,90 +1,64 @@
 #include <bits/stdc++.h>
-#define pb push_back
-#define mp make_pair
-#define ll long long int
-#define ff first
-#define ss second
-#define S size()
-#define mod (ll)(1e9 + 7)
-#define mset(a, k) memset(a, k, sizeof(a))
-#define fr(i, x, y) for (ll i = x; i < y; i++)
-#define dr(i, x, y) for (ll i = x; i >= y; i--)
-#define all(v) v.begin(), v.end()
-#define allr(v) v.rbegin(), v.rend()
-#define mapcl map<char, ll>
-#define mapll map<ll, ll>
-#define mapsl map<string, ll>
-#define vi vector<ll>
-#define vs vector<string>
-#define vb vector<bool>
-#define psi pair<string, ll>
-#define pii pair<ll, ll>
-#define piii pair<ll, pii>
-#define vii vector<pii>
-#define vvi vector<vi>
-#define vvii vector<vii>
 using namespace std;
-void fast()
+bool isItSafe(vector<vector<bool>> &arr, int row, int col, int n)
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-}
-//----------------------------------------FUNCTIONS-------------------------------------
-const ll N = (ll)(1 * 1e6 + 5);
-ll qcount;
-ll n;
-bitset < 100> col, ld, rd;
-void displayGrid(vvi &grid, ll n)
-{
-	fr(i, 0, n)
+	for (int i = row - 1; i >= 0; i--)
 	{
-		fr(j, 0, n)
-		{
-			if (grid[i][j])cout << "Q\t";
-			else cout << "-\t";
-		}
-		cout << '\n';
+		if (arr[i][col] == true)
+			return false;
 	}
-	cout << '\n';
+	for (int i = row - 1, j = col - 1; i >= 0 and j >= 0; i--, j--)
+		if (arr[i][j] == true)
+			return false;
+	for (int i = row - 1, j = col + 1; i >= 0 and j < n; i--, j++)
+		if (arr[i][j] == true)
+			return false;
+	return true;
 }
-void Nqueen(ll curr_row, vvi &grid)
+int countWays = 0;
+void display(vector<vector<bool>> &arr, int n)
 {
-	if (curr_row == n) {
-		qcount++;
-		displayGrid(grid, n);
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (arr[i][j])
+			{
+				cout << "Q ";
+			}
+			else
+			{
+				cout << ". ";
+			}
+				}
+		cout << endl;
+	}
+}
+void countNQueen(vector<vector<bool>> &arr, int row, int n)
+{
+	if (row == n)
+	{
+		countWays++;
+		display(arr, n);
+		cout << "\n\n";
 		return;
 	}
-	fr(i, 0, n) {
-		if (col[i] || rd[i + curr_row] || ld[i - curr_row + n - 1])continue;
-		col[i] = rd[i + curr_row] = ld[i - curr_row + n - 1] = grid[curr_row][i] = 1;
-		Nqueen(curr_row + 1, grid);
-		col[i] = rd[i + curr_row] = ld[i - curr_row + n - 1] = grid[curr_row][i] = 0;
-	}
-}
-void solve()
-{
-	//ll n;
-	cin >> n;
-	qcount = 0;
-	vvi grid(n, vi(n, 0));
-	Nqueen(0, grid);
-	if (qcount == 0)cout << "Not Possible\n";
-}
-
-signed main()
-{
-	fast();
-#ifndef ONLINE_JUDGE
-	freopen ("inp.txt", "r", stdin);
-	freopen ("out.txt", "w", stdout);
-#endif
-	ll t = 1;
-	//ll t;
-	fr(i, 0, t)
+	for (int i = 0; i < n; i++)
 	{
-		//cout << t + 1 << ".\n";
-		solve();
-		//cout << qcount << '\n';
+		if (isItSafe(arr, row, i, n))
+		{
+			arr[row][i] = true;
+			countNQueen(arr, row + 1, n);
+			arr[row][i] = false;
+		}
 	}
+}
+int main()
+{
+	int n;
+	cin >> n;
+	vector<vector<bool>> arr(n, vector<bool>(n));
+	countNQueen(arr, 0, n);
+	cout << countWays << endl;
+	return 0;
 }
