fd9e3cc9a146d739f6c901a245d2c66533cdad93
Bhanu Prakash <46644862+prabhanukash@users.noreply.github.com>
2024-07-06 03:22:27 +0000

    lds changes

--- DP/lds.cpp
+++ DP/lds.cpp
@@ -1,81 +1,29 @@
-// Problem Link -
-/* By Bhanu Prakash */
-#include <bits/stdc++.h>
-//#include<ext/pb_ds/assoc_container.hpp>
-//#include<ext/pb_ds/tree_policy.hpp>
-//#include <ext/pb_ds/trie_policy.hpp>
-//using namespace __gnu_pbds;
-#define pb push_back
-#define mp make_pair
-#define ll long long int
-#define ff first
-#define ss second
-#define S size()
-#define mod (ll)(1e9 + 7)
-#define inf 1e18
-#define fr(i, x, y) for (ll i = x; i < y; i++)
-#define dr(i, x, y) for (ll i = x; i >= y; i--)
-#define all(v) v.begin(), v.end()
-#define allr(v) v.rbegin(), v.rend()
-#define mapcl map<char, ll>
-#define mapll map<ll, ll>
-#define vi vector<ll>
-#define vs vector<string>
-#define vb vector<bool>
-#define psi pair<string, ll>
-#define pii pair<ll, ll>
-#define piii pair<ll, pii>
-#define vii vector<pii>
-#define vvi vector<vi>
-#define vvii vector<vii>
+#include <iostream>
+#include <vector>
 using namespace std;
-//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
-//typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
-void fast()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-}
-//----------------------------------------FUNCTIONS-------------------------------------
-const ll N = (ll)(1 * 1e6 + 5);
 
-void solve()
-{
-    ll n;
-    cin >> n;
-    ll a[n];
-    fr(i, 0, n) cin >> a[i];
-    ll dp[n];
+int longestDecreasingSubsequence(int arr[], int n) {
+    vector<int> dp(n, 0);
     dp[0] = 1;
-    ll ans = INT_MIN;
-    fr(i, 1, n)
-    {
-        ll mx = 0;
-        fr(j, 0, i)
-        {
-            if (a[j] > a[i])
-            {
+    int ans = 1; 
+
+    for (int i = 1; i < n; i++) {
+        int mx = 0;
+        for (int j = 0; j < i; j++) {
+            if (arr[j] > arr[i]) { 
                 mx = max(dp[j], mx);
             }
         }
         dp[i] = mx + 1;
         ans = max(ans, dp[i]);
     }
-    cout << ans << '\n';
+
+    return ans;
 }
 
-signed main()
-{
-    fast();
-#ifndef ONLINE_JUDGE
-    freopen("inp.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
-#endif
-    ll t = 1;
-    //cin >> t;
-    fr(i, 0, t)
-    {
-        solve();
-    }
+int main() {
+    int arr[] = {10, 22, 9, 33, 21, 50, 41, 40, 20};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout << "Length of LDS is " << longestDecreasingSubsequence(arr, n) << endl;
+    return 0;
 }
