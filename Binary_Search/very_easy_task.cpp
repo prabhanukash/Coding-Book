1158d452c37e93f45a48bdd2e8e60fa3df8a46a1
Bhanu Prakash <46644862+prabhanukash@users.noreply.github.com>
2024-07-07 03:18:21 +0000

    commented the logic

--- Binary_Search/very_easy_task.cpp
+++ Binary_Search/very_easy_task.cpp
@@ -3,11 +3,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 long long n, x, y;
-bool good(long long val)
-{
-	if (val < min(x, y)) return false;
-	val -= min(x, y);
-	return ((val / x) + (val / y) + 1 >= n);
+// Function to check if `val` time is enough to produce `n` copies
+bool good(long long val) {
+		if (val < min(x, y)) return false; // If `val` is less than the time taken by the faster copier
+		val -= min(x, y); // First copy is done by the faster copier
+		return ((val / x) + (val / y) + 1 >= n); // Check if total copies including the first one is at least `n`
 }
 int main()
 {
