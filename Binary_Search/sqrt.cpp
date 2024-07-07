a2b1fed45993b43c87f051f71bfd95016d1377d6
Bhanu Prakash <46644862+prabhanukash@users.noreply.github.com>
2024-07-07 03:31:03 +0000

    commented the logic

--- Binary_Search/sqrt.cpp
+++ Binary_Search/sqrt.cpp
@@ -1,41 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Solution
-{
+
+class Solution {
 private:
-	bool good(int n, long long val)
-	{
-		long long sqrt = val * val;
-		return sqrt <= n;
-	}
+  // Function to check if `val * val` is less than or equal to `n`
+  bool good(int n, long long val) {
+    long long sqrt = val * val;
+    return sqrt <= n;
+  }
 
 public:
-	int mySqrt(int x)
-	{
-		if (x <= 1)
-			return x;
-		int l = 0;
-		int r = x;
-		while (r > l + 1)
-		{
-			long long m = (l + r) >> 1;
-			if (good(x, m))
-			{
-				l = m;
-			}
-			else
-			{
-				r = m;
-			}
-		}
-		return l;
-	}
+  int mySqrt(int x) {
+    if (x <= 1) // Handle edge cases where x is 0 or 1
+      return x;
+
+    int l = 0; // good
+    int r = x; // bad
+
+    while (r > l + 1) {
+      long long m = (l + r) >> 1;
+      if (good(x, m)) {
+        l = m;
+      } else {
+        r = m;
+      }
+    }
+    return l;
+  }
 };
 
-int main(){
-	Solution solution;
-	int n;
-	cin >> n;
-	int ans = solution.mySqrt(n);
-	cout << ans;
+int main() {
+  Solution solution;
+  int n;
+  cin >> n;
+  int ans = solution.mySqrt(n);
+  cout << ans;
 }
