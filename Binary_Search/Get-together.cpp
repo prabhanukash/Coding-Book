c1aa970dc8bd95558d0da643362047cfb7c0cd4e
Bhanu Prakash <46644862+prabhanukash@users.noreply.github.com>
2024-07-07 05:14:35 +0000

    commented with logic

--- Binary_Search/Get-together.cpp
+++ Binary_Search/Get-together.cpp
@@ -1,53 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct citizen
-{
-    int x_position, speed;
+struct citizen {
+  int x_position, speed;
 };
 
-bool good(vector<citizen> &citizens, int n, double time)
-{
-    double L = -1e10;
-    double R = 1e10;
-    for (int i = 0; i < n; i++)
-    {
-        double xl = citizens[i].x_position - time * citizens[i].speed;
-        double xr = citizens[i].x_position + time * citizens[i].speed;
+// Function to check if all citizens can gather within 'time' seconds
+bool good(vector<citizen> &citizens, int n, double time) {
+  double L = -1e10;
+  double R = 1e10;
+  for (int i = 0; i < n; i++) {
+    double xl = citizens[i].x_position - time * citizens[i].speed;
+    double xr = citizens[i].x_position + time * citizens[i].speed;
 
-        L = max(L, xl);
-        R = min(R, xr);
+    L = max(L, xl);
+    R = min(R, xr);
 
-        if (L > R)
-        {
-            return false;
-        }
+    if (L > R) {
+      return false;
     }
+  }
 
-    return true;
+  return true;
 }
-int main()
-{
-    int n;
-    cin >> n;
-    vector<citizen> citizens(n);
+int main() {
+  int n;
+  cin >> n;
+  vector<citizen> citizens(n);
 
-    for (auto &citizen : citizens)
-        cin >> citizen.x_position >> citizen.speed;
+  for (auto &citizen : citizens)
+    cin >> citizen.x_position >> citizen.speed;
 
-    double l = 0;
-    double r = 1e18;
-    for (int i = 0; i < 100; i++)
-    {
-        double m = (l + r) / 2;
-        if (good(citizens, n, m))
-        {
-            r = m;
-        }
-        else
-        {
-            l = m;
-        }
+  double l = 0;
+  double r = 1e18;
+  for (int i = 0; i < 100; i++) {
+    double m = (l + r) / 2;
+    if (good(citizens, n, m)) {
+      r = m;
+    } else {
+      l = m;
     }
-    cout << setprecision(10) << r << endl;
+  }
+  cout << setprecision(10) << r << endl;
 }
+
+// Time Complexity: O(nlogM)
+// Space Complexity: O(n)
