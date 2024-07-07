9b9eaffab79d1c3fadeea3b085cb0690f2993547
Bhanu Prakash <46644862+prabhanukash@users.noreply.github.com>
2024-07-07 04:36:01 +0000

    commented with logic

--- Binary_Search/packing-rectangles.cpp
+++ Binary_Search/packing-rectangles.cpp
@@ -2,30 +2,39 @@
 /* By Bhanu Prakash */
 #include <bits/stdc++.h>
 using namespace std;
+
 long long w, h, n;
-bool good(long long x)
-{
+
+// Function to check if a square of side 'x' can fit at least 'n' rectangles of size 'w' x 'h'
+bool good(long long x) {
     return (x / w) * (x / h) >= n;
 }
-int main()
-{
+
+int main() {
     ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
     cin >> w >> h >> n;
 
-    long long l = 0;
-    long long r = 1;
-    while(!good(r))
+    long long l = 0; // bad
+    long long r = 1; // good
+
+    while (!good(r)) {
         r *= 2;
-    while (r > l + 1)
-    {
-        long long m = (l + r) / 2;
-        if (good(m))
-        {
-            r = m;
-        }
-        else
-            l = m;
     }
+
+    while (r > l + 1) {
+        long long m = (l + r) / 2;  
+        if (good(m)) {
+            r = m;  
+        } else {
+            l = m; 
+        }
+    }
+
     cout << r << endl;
     return 0;
 }
+// Time Complexity: O(log(max side length))
+// Space Complexity: O(1)
