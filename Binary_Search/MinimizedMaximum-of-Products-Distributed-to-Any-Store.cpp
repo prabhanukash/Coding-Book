7af4c5a0d22ff4bc0a9ada94f023cfa23207d769
Bhanu Prakash <46644862+prabhanukash@users.noreply.github.com>
2024-07-07 04:55:13 +0000

    commented with logic

--- Binary_Search/MinimizedMaximum-of-Products-Distributed-to-Any-Store.cpp
+++ Binary_Search/MinimizedMaximum-of-Products-Distributed-to-Any-Store.cpp
@@ -1,49 +1,47 @@
+//https://leetcode.com/problems/minimized-maximum-of-products-distributed-to-any-store/description/
 #include <bits/stdc++.h>
 using namespace std;
-class Solution
-{
+
+class Solution {
 private:
-    bool good(vector<int> &quantities, int x, int n)
-    {
+    // Function to check if it's possible to distribute products such that max is <= x
+    bool good(vector<int>& quantities, int x, int n) {
         int cnt = 0;
-        for (int val : quantities)
-        {
+        for (int val : quantities) {
             cnt += ceil((double)val / (double)x);
         }
         return cnt <= n;
     }
 
 public:
-    int minimizedMaximum(int n, vector<int> &quantities)
-    {
+    int minimizedMaximum(int n, vector<int>& quantities) {
         int l = 1;
         int r = *max_element(quantities.begin(), quantities.end());
         int ans = 0;
-        while (l <= r)
-        {
-            int m = (l + r) >> 1;
-            if (good(quantities, m, n))
-            {
-                ans = m;
-                r = m - 1;
-            }
-            else
-            {
-                l = m + 1;
+
+       
+        while (l <= r) {
+            int m = (l + r) >> 1; 
+            if (good(quantities, m, n)) {
+                ans = m; 
+                r = m - 1; 
+            } else {
+                l = m + 1; 
             }
         }
+
         return ans;
     }
 };
-int main()
-{
+
+int main() {
     Solution solution;
     int n, m;
     cin >> n >> m;
     vector<int> quantities(m);
     for (int i = 0; i < m; i++)
-        cin >> quantities[i];
+        cin >> quantities[i]; 
     int ans = solution.minimizedMaximum(n, quantities);
-    cout << ans << endl;
+    cout << ans << endl; 
     return 0;
 }
